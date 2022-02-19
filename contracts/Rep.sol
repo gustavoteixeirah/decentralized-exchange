8417c2b178e3624d262ba9d48e1165e5465fa9bb
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-02-19 16:00:43 -0300

    Added more fake tokens and fixed contract names

--- contracts/Rep.sol
+++ contracts/Rep.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Dai is ERC20 {
-    constructor(uint256 initialSupply) ERC20("DAI", "Dai Stablecoin") {
+contract Rep is ERC20 {
+    constructor(uint256 initialSupply) ERC20("REP", "Augur token") {
         _mint(msg.sender, initialSupply);
     }
 }
--- contracts/Zrx.sol
+++ contracts/Zrx.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Dai is ERC20 {
-    constructor(uint256 initialSupply) ERC20("DAI", "Dai Stablecoin") {
+contract Zrx is ERC20 {
+    constructor(uint256 initialSupply) ERC20("ZRX", "0x token") {
         _mint(msg.sender, initialSupply);
     }
 }
